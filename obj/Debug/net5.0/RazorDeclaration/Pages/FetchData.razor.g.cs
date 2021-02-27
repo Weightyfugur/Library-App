7190eefac9ff8453b11b5a6a31c5aff4803d48ac
ConnerLoveless <52382895+ConnerLoveless@users.noreply.github.com>
2021-02-27 18:31:21 -0500

    Just Branch

--- obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -13,77 +13,77 @@ namespace LibraryApp.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "Z:\Swango\Desktop\GitHub Repositories\Library-App\_Imports.razor"
+#line 1 "C:\Users\calan\Documents\GitHub\library\Library-App\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "Z:\Swango\Desktop\GitHub Repositories\Library-App\_Imports.razor"
+#line 2 "C:\Users\calan\Documents\GitHub\library\Library-App\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "Z:\Swango\Desktop\GitHub Repositories\Library-App\_Imports.razor"
+#line 3 "C:\Users\calan\Documents\GitHub\library\Library-App\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "Z:\Swango\Desktop\GitHub Repositories\Library-App\_Imports.razor"
+#line 4 "C:\Users\calan\Documents\GitHub\library\Library-App\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "Z:\Swango\Desktop\GitHub Repositories\Library-App\_Imports.razor"
+#line 5 "C:\Users\calan\Documents\GitHub\library\Library-App\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "Z:\Swango\Desktop\GitHub Repositories\Library-App\_Imports.razor"
+#line 6 "C:\Users\calan\Documents\GitHub\library\Library-App\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "Z:\Swango\Desktop\GitHub Repositories\Library-App\_Imports.razor"
+#line 7 "C:\Users\calan\Documents\GitHub\library\Library-App\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "Z:\Swango\Desktop\GitHub Repositories\Library-App\_Imports.razor"
+#line 8 "C:\Users\calan\Documents\GitHub\library\Library-App\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "Z:\Swango\Desktop\GitHub Repositories\Library-App\_Imports.razor"
+#line 9 "C:\Users\calan\Documents\GitHub\library\Library-App\_Imports.razor"
 using LibraryApp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "Z:\Swango\Desktop\GitHub Repositories\Library-App\_Imports.razor"
+#line 10 "C:\Users\calan\Documents\GitHub\library\Library-App\_Imports.razor"
 using LibraryApp.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "Z:\Swango\Desktop\GitHub Repositories\Library-App\Pages\FetchData.razor"
+#line 3 "C:\Users\calan\Documents\GitHub\library\Library-App\Pages\FetchData.razor"
 using LibraryApp.Data;
 
 #line default
@@ -98,7 +98,7 @@ using LibraryApp.Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 39 "Z:\Swango\Desktop\GitHub Repositories\Library-App\Pages\FetchData.razor"
+#line 39 "C:\Users\calan\Documents\GitHub\library\Library-App\Pages\FetchData.razor"
        
     private WeatherForecast[] forecasts;
 

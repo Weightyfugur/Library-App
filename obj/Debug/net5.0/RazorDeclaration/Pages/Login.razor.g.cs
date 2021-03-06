9aa0314ccc92e9c68444fc59bb62a57ae20c9456
ConnerLoveless <52382895+ConnerLoveless@users.noreply.github.com>
2021-03-06 14:55:51 -0500

    Added in CSS Changes, New Page
    I added in some aesthetic changes to the CSS for the Search and Addition/Deletion Pages as well as added in a Login Page!

--- obj/Debug/net5.0/RazorDeclaration/Pages/Login.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Login.razor.g.cs
@@ -82,22 +82,14 @@ using LibraryApp.Shared;
 #line default
 #line hidden
 #nullable disable
-    [Microsoft.AspNetCore.Components.RouteAttribute("/search")]
-    public partial class Search : Microsoft.AspNetCore.Components.ComponentBase
+    [Microsoft.AspNetCore.Components.RouteAttribute("/login")]
+    public partial class Login : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
         {
         }
         #pragma warning restore 1998
-#nullable restore
-#line 24 "C:\Users\calan\Documents\GitHub\library\Library-App\Pages\Search.razor"
-       
-	private string bookName = "Enter Book Name Here";
-
-#line default
-#line hidden
-#nullable disable
     }
 }
 #pragma warning restore 1591
